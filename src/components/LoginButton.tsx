aee0032bb5b89616b19c8cbc39c4774008762a57
brokoyy <bz523533@gmail.com>
2025-08-23 07:02:23 +0900

    Update LoginButton.tsx

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { loginWithNip07, loginWithNsecApp } from "../api/auth";
 
 export default function Login() {
   return (
     <div>
+      <h2>ログイン</h2>
       <button onClick={loginWithNip07}>NIP-07でログイン</button>
       <button onClick={loginWithNsecApp}>nsec.appでログイン</button>
     </div>
