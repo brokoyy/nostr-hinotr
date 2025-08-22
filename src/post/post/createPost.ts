eb8ec3ed508e16f8f65ce06f3b1ede668cc4bcd8
brokoyy <bz523533@gmail.com>
2025-08-22 22:11:09 +0900

    投稿

---
+++ src/post/post/createPost.ts
@@ -0,0 +1,27 @@
+import { relayInit, getEventHash, NostrEvent } from "nostr-tools";
+
+const RELAYS = ["wss://r.kojira.io", "wss://yabu.me"];
+
+export async function createPost(pubkey: string, content: string) {
+  if (!window.nostr) throw new Error("NIP-07 extension not found");
+
+  let event: NostrEvent = {
+    kind: 1,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [],
+    content,
+    pubkey
+  };
+
+  event.id = getEventHash(event);
+  const signedEvent = await window.nostr.signEvent(event);
+
+  RELAYS.forEach(async (url) => {
+    const relay = relayInit(url);
+    await relay.connect();
+
+    const pub = relay.publish(signedEvent);
+    pub.on("ok", () => console.log(`Post published to ${url}`));
+    pub.on("failed", () => console.log(`Failed to publish to ${url}`));
+  });
+}
