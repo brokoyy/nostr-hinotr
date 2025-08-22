a731b0309a4793a3e078f9b7f2111a9adf6845f4
brokoyy <bz523533@gmail.com>
2025-08-22 22:14:49 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,23 @@
+import React, { useState } from "react";
+import { LoginButton } from "./components/LoginButton";
+import { TimelineList } from "./components/TimelineList";
+import { PostForm } from "./components/PostForm";
+import { useTimeline } from "./timeline/useTimeline";
 
+export const App: React.FC = () => {
+  const [pubkey, setPubkey] = useState<string | null>(null);
+  const events = useTimeline();
+
+  return (
+    <div style={{ padding: 20 }}>
+      {!pubkey ? (
+        <LoginButton onLogin={setPubkey} />
+      ) : (
+        <>
+          <PostForm pubkey={pubkey} />
+          <TimelineList events={events} />
+        </>
+      )}
+    </div>
+  );
+};
