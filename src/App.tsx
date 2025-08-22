757052ad58e23f4efe0aa3a675097d617ddf1ea2
brokoyy <bz523533@gmail.com>
2025-08-23 08:00:52 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Timeline from "./components/TimelineList";
 import LoginButton from "./components/LoginButton";
 import Callback from "./pages/auth/callback";
 
 function App() {
+  const pubkey = localStorage.getItem("pubkey");
+
   return (
     <Router>
-      <div className="App">
-        <h1>Hinotr (Nostr Client)</h1>
-        <LoginButton />
+      <div className="App p-4">
+        <h1 className="text-2xl font-bold mb-6">Hinotr (Nostr Client)</h1>
+
         <Routes>
-          <Route path="/" element={<Timeline />} />
+          {/* ホームはログイン済みならタイムライン、未ログインならログインボタン */}
+          <Route
+            path="/"
+            element={pubkey ? <Timeline /> : <LoginButton />}
+          />
+
+          {/* nsec.app からのコールバック */}
           <Route path="/callback" element={<Callback />} />
+
+          {/* それ以外はホームにリダイレクト */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
