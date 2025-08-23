56a3c1f38dcae18dd456501d61639c92885f9afd
brokoyy <bz523533@gmail.com>
2025-08-23 23:36:41 +0900

    Update TimelineList.tsx

--- src/components/TimelineList.tsx
+++ src/components/TimelineList.tsx
@@ -2,10 +2,23 @@ import React from "react";
 
 export const TimelineList = ({ events }: { events: any[] }) => {
   return (
-    <ul>
+    <ul style={{ listStyle: "none", padding: 0 }}>
       {events.map((e, i) => (
-        <li key={i}>
-          <img src={e.profile.picture || ""} alt="icon" width={32} />
+        <li
+          key={i}
+          style={{
+            borderBottom: "1px solid #ccc",
+            padding: 10,
+            marginBottom: 5
+          }}
+        >
+          <img
+            src={e.profile.picture || ""}
+            alt="icon"
+            width={32}
+            height={32}
+            style={{ borderRadius: "50%", verticalAlign: "middle", marginRight: 10 }}
+          />
           <strong>{e.profile.name || e.pubkey}</strong>: {e.content}
         </li>
       ))}
