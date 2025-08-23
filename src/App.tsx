20354c700c3e10e65d4a23ff140bc06ca7d4029d
brokoyy <bz523533@gmail.com>
2025-08-23 21:41:36 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { PostForm } from "./components/PostForm";
 import { TimelineList } from "./components/TimelineList";
-import { useTimeline } from "./hooks/useTimeline";
+import { useTimeline } from "./timeline/useTimeline";
 
 export default function App() {
   const [pubkey, setPubkey] = useState<string | null>(null);
