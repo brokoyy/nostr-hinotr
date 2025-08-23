27779473849b3585ca3700bef5e81da85ea5f4ba
brokoyy <bz523533@gmail.com>
2025-08-23 21:09:48 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "./App";      // App.tsx は同じ src 内
+import "./index.css";        // index.css も同じ src 内
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
