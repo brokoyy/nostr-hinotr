390854d70b6de3d65fd675e2a6a288882cc5676c
brokoyy <bz523533@gmail.com>
2025-08-23 23:33:54 +0900

    Update fetchProfile.ts

--- src/api/fetchProfile.ts
+++ src/api/fetchProfile.ts
@@ -4,18 +4,20 @@ const RELAYS = ["wss://yabu.me/", "wss://r.kojira.io/"];
 
 export async function fetchProfile(pubkey: string) {
   const profiles: Record<string, any> = {};
-  for (const url of RELAYS) {
-    const relay = relayInit(url);
-    await relay.connect();
-    const filter: Filter = { kinds: [0], authors: [pubkey] };
-    relay.on("event", (event) => {
-      try {
-        const content = JSON.parse(event.content);
-        profiles[pubkey] = { name: content.name, picture: content.picture };
-      } catch {}
-    });
-    relay.sub([filter]);
-    setTimeout(() => relay.close(), 2000);
-  }
+  await Promise.all(
+    RELAYS.map(async (url) => {
+      const relay = relayInit(url);
+      await relay.connect();
+      const filter: Filter = { kinds: [0], authors: [pubkey] };
+      relay.on("event", (event) => {
+        try {
+          const content = JSON.parse(event.content);
+          profiles[pubkey] = { name: content.name, picture: content.picture };
+        } catch {}
+      });
+      relay.sub([filter]);
+      setTimeout(() => relay.close(), 2000);
+    })
+  );
   return profiles[pubkey] || { name: pubkey, picture: "" };
 }
