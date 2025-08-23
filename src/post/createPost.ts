db2a4934fb050cfe7fb3938b5167cddf9500a2b6
brokoyy <bz523533@gmail.com>
2025-08-23 21:22:18 +0900

    Update createPost.ts

--- src/post/createPost.ts
+++ src/post/createPost.ts
@@ -1,26 +1,25 @@
-import * as nostr from "nostr-tools";
+import { getEventHash, NostrEvent, SimplePool } from "nostr-tools";
 
 const RELAYS = ["wss://r.kojira.io", "wss://yabu.me"] as const;
 
 export async function createPost(pubkey: string, content: string) {
   if (!window.nostr) throw new Error("NIP-07 extension not found");
 
-  const event: nostr.NostrEvent = {
+  const event: NostrEvent = {
     kind: 1,
     created_at: Math.floor(Date.now() / 1000),
     tags: [],
     content,
     pubkey,
+    id: "",
   };
 
-  event.id = nostr.getEventHash(event);
+  event.id = getEventHash(event);
   const signedEvent = await window.nostr.signEvent(event);
 
-  for (const url of RELAYS) {
-    const relay = nostr.relayInit(url);
-    await relay.connect();
-    const pub = relay.publish(signedEvent);
-    pub.on("ok", () => console.log(`Post published to ${url}`));
-    pub.on("failed", () => console.log(`Failed to publish to ${url}`));
-  }
+  const pool = new SimplePool();
+  const pub = pool.publish(RELAYS, signedEvent);
+
+  pub.on("ok", (relayUrl) => console.log(`Published to ${relayUrl}`));
+  pub.on("failed", (relayUrl) => console.log(`Failed to publish to ${relayUrl}`));
 }
